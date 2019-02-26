09b7b9394438f80c3a4739b12f8d69582fc93a8d
Amanda Friedman <amandalibre421@gmail.com>
2019-02-25 23:35:23 -0500

    add faux-filtering

--- posts/urls.py
+++ posts/urls.py
@@ -5,6 +5,10 @@ app_name = 'posts'
 
 urlpatterns = [
     url(r'^$', views.post_list, name="list"),
+    url(r'^11$', views.post_list_11, name="list_11"),
+    url(r'^12$', views.post_list_12, name="list_12"),
+    url(r'^13$', views.post_list_13, name="list_13"),
+    url(r'^plus$', views.post_list_plus, name="list_plus"),
     url(r'^create/$', views.post_create, name="create"),
-    url(r'^P<slug>[\w-]+/$', views.post_detail, name="detail")
+    url(r'^P<slug>[\w-]+/$', views.post_detail, name="detail"),
 ]
--- posts/views.py
+++ posts/views.py
@@ -9,6 +9,26 @@ def post_list(request):
     return render(request, "posts/post_list.html", {"posts":posts})
 
 
+def post_list_11(request):
+    posts_11 = Post.objects.filter(size__exact='11').order_by("date")
+    return render(request, "posts/post_list_11.html", {"posts_11":posts_11})
+
+
+def post_list_12(request):
+    posts_12 = Post.objects.filter(size__exact='12').order_by("date")
+    return render(request, "posts/post_list_12.html", {"posts_12":posts_12})
+
+
+def post_list_13(request):
+    posts_13 = Post.objects.filter(size__exact='13').order_by("date")
+    return render(request, "posts/post_list_13.html", {"posts_13":posts_13})
+
+
+def post_list_plus(request):
+    posts_plus = Post.objects.filter(size__exact='plus').order_by("date")
+    return render(request, "posts/post_list_plus.html", {"posts_plus":posts_plus})
+
+
 def post_detail(request, slug):
     post = Post.objects.get(slug=slug)
     return render(request, 'posts/post_detail.html', {'post': post})
